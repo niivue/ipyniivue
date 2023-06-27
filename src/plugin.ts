4fb5cccc80090f63969b4ed7a3379549d681e318
Anthony Androulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2023-06-27 17:33:22 -0400

    update author

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,4 @@
-// Copyright (c) Anthony Androulakis
+// Copyright (c) Niivue
 // Distributed under the terms of the Modified BSD License.
 
 import { Application, IPlugin } from '@lumino/application';
