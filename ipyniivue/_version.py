0e18263b0de8edf2945cb98ed1121e08823e00c9
Anthony Androulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2023-07-23 14:47:54 -0400

    remove dev from version string

--- ipyniivue/_version.py
+++ ipyniivue/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Niivue.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (1, 1, 0, 'dev')
+version_info = (1, 1, 0)
 __version__ = ".".join(map(str, version_info))
