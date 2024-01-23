529d8a3747a5a6a587205df016ab3d0b40190798
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-23 22:57:29 +0100

    slider example

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+import "./widget.css";
+
+import { Niivue } from "@niivue/niivue";
+
+export async function render({ model, el }) {
+  let canvas = document.createElement("canvas");
+  let container = document.createElement("div");
+  container.style.height = "300px";
+  container.appendChild(canvas);
+  el.appendChild(container);
+  let nv = new Niivue();
+  nv.attachToCanvas(canvas);
+  await nv.loadVolumes([
+    { url: "https://niivue.github.io/niivue/images/mni152.nii.gz" },
+  ]);
+
+  nv.setOpacity(0, 0.2);
+
+  model.on("change:opacity", () => {
+    console.log("opacity changed");
+    let value = model.get("opacity");
+    console.log(value);
+    nv.setOpacity(0, value)
+  });
+
+
+
+}
