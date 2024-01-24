874a7fd8491b601aa13293cafb5dd3046dd262ba
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-24 11:08:30 +0100

    small tweaks

--- src/ipyniivue_experimental/__init__.py
+++ src/ipyniivue_experimental/__init__.py
@@ -9,8 +9,3 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-
-class Counter(anywidget.AnyWidget):
-    _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
-    _css = pathlib.Path(__file__).parent / "static" / "widget.css"
-    value = traitlets.Int(0).tag(sync=True)
