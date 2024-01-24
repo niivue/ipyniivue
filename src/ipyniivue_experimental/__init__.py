ce4e995ab51e9b4527e3fcaa680bbcc57adc7fed
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-24 11:31:12 +0100

    add demo

--- src/ipyniivue_experimental/__init__.py
+++ src/ipyniivue_experimental/__init__.py
@@ -9,3 +9,18 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
+############ This code block will go to __init__.py 
+
+import anywidget 
+import pathlib
+
+class AnyNiivue(anywidget.AnyWidget):
+  path_root = pathlib.Path(__file__).parent.parent.parent
+  _esm = path_root / "src" / "ipyniivue_experimental"/ "static" / "widget_send.js"
+
+  def load_volumes(self, volume_list):
+    self.send({ "type": "api", "func": "loadVolumes", "args": [volume_list] })
+
+  def load_meshes(self, mesh_list):
+    self.send({ "type": "api", "func": "loadMeshes", "args": [mesh_list] })
+############
