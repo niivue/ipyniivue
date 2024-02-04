6c5e9f21a03d62a595fecfb41fe6cdc524daedfd
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-04 16:54:20 +0100

    simplify

--- js/widget_send.js
+++ js/widget_send.js
@@ -10,29 +10,9 @@ export async function render({ model, el }) {
   el.appendChild(container);
   let nv = new Niivue();
   nv.attachToCanvas(canvas);
-  // nv.loadVolumes([
-  //   { url: "https://niivue.github.io/niivue/images/mni152.nii.gz" },
-  // ]);
-  model.on("msg:custom", (msg) => {
-    if (msg.type === "setting_property") {
-      if (msg.func === "setColorMap") {
-        let mapping = {
-          "nv.volumes[0].id": nv.volumes[0].id,
-        };
-        let funcname = msg.func;
-        let [arg1, arg2] = msg.args;
-        let arg1mapped = mapping[arg1];
 
-        nv[funcname](arg1mapped, arg2);
-        // smame as nv[funcname](nv.volumes[0].id, "green");
-        console.log("Setting property message received!");
-        return;
-      }
-      // setOpacity, setCrosshairColor , ...
-      nv[msg.func](...msg.args);
-    }
+  model.on("msg:custom", (msg) => {
     if (msg.type === "api") {
-      // API call for load volume, load mesh etc.
       console.log("API message received!");
       let funcname = msg.func;
       nv[funcname](...msg.args);
