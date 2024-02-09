39f307259e50a9f3e3d7ab755e473ae3614ba220
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-09 14:07:30 +0100

    add traitet approach

--- js/widget_traitlet.js
+++ js/widget_traitlet.js
@@ -10,16 +10,29 @@ async function render({ model, el }) {
   el.appendChild(container);
   let nv = new Niivue();
   nv.attachToCanvas(canvas);
-  await nv.loadVolumes([
-    { url: "https://niivue.github.io/niivue/images/mni152.nii.gz" },
-  ]);
-  // nv.setColorMap(0,"red")
+  // await nv.loadVolumes([
+  //   { url: "https://niivue.github.io/niivue/images/mni152.nii.gz" },
+  // ]);
+  // nv.setOpacity(0, model.get("opacity"));
+
+  model.on("change:volume", () => {
+    console.log("volume changed");
+    let value = model.get("volume");
+    console.log(value);
+    nv.loadVolumes(value);
+  });
+
   model.on("change:opacity", () => {
     console.log("opacity changed");
     let value = model.get("opacity");
     console.log(value);
     nv.setOpacity(0, value);
   });
-}
 
-export default { render }
+  // model.on("change:colormap", () => {
+  //   console.log("colormap changed");
+  //   let value = model.get("colormap");
+  //   console.log(value);
+  //   nv.setColorMap(nv.volumes[0].id, value);
+  // });
+}
