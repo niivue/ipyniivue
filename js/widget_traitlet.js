892739cce17e2817a86763d3b20c26bbe4ae11de
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-10 11:07:29 +0000

    using aync

--- js/widget_traitlet.js
+++ js/widget_traitlet.js
@@ -2,37 +2,23 @@ import "./widget.css";
 
 import { Niivue } from "@niivue/niivue";
 
-async function render({ model, el }) {
+export async function render({ model, el }) {
   let canvas = document.createElement("canvas");
   let container = document.createElement("div");
   container.style.height = "300px";
   container.appendChild(canvas);
   el.appendChild(container);
   let nv = new Niivue();
+  let value = model.get("volume");
   nv.attachToCanvas(canvas);
-  // await nv.loadVolumes([
-  //   { url: "https://niivue.github.io/niivue/images/mni152.nii.gz" },
-  // ]);
-  // nv.setOpacity(0, model.get("opacity"));
-
-  model.on("change:volume", () => {
-    console.log("volume changed");
-    let value = model.get("volume");
-    console.log(value);
+  if (value != "") {
     nv.loadVolumes(value);
-  });
+  }
 
-  model.on("change:opacity", () => {
-    console.log("opacity changed");
-    let value = model.get("opacity");
+  model.on("change:volume", async () => {
+    value = model.get("volume");
+    console.log("volume changed");
     console.log(value);
-    nv.setOpacity(0, value);
+    await nv.loadVolumes(value);
   });
-
-  // model.on("change:colormap", () => {
-  //   console.log("colormap changed");
-  //   let value = model.get("colormap");
-  //   console.log(value);
-  //   nv.setColorMap(nv.volumes[0].id, value);
-  // });
 }
