fea66dd543398c42a5fd701b46239fdf4e02e928
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-12 14:34:58 +0000

    add SLICE_TYPE and dragAndDrop flase

--- js/widget_traitlet.js
+++ js/widget_traitlet.js
@@ -1,14 +1,17 @@
 import "./widget.css";
 
-import { Niivue } from "@niivue/niivue";
+import { Niivue, SLICE_TYPE } from "@niivue/niivue";
 
 export async function render({ model, el }) {
+  // let options = model.get("options");
+
+  const options = {dragAndDropEnabled: false} 
   let canvas = document.createElement("canvas");
   let container = document.createElement("div");
   container.style.height = "300px";
   container.appendChild(canvas);
   el.appendChild(container);
-  let nv = new Niivue();
+  let nv = new Niivue(options);
   let value = model.get("volume");
   nv.attachToCanvas(canvas);
   if (value != "") {
@@ -17,8 +20,10 @@ export async function render({ model, el }) {
 
   model.on("change:volume", async () => {
     value = model.get("volume");
+    
     console.log("volume changed");
     console.log(value);
     await nv.loadVolumes(value);
+    nv.setSliceType(SLICE_TYPE.MULTIPLANAR)
   });
 }
