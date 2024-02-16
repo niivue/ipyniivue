c7088cfd146a4595f2c053518769b699057274fe
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-02-16 14:42:17 +0000

    tweaks

--- js/widget_vscode.js
+++ js/widget_vscode.js
@@ -21,6 +21,35 @@ async function render({ model, el }) {
     console.log(value);
     await nv.loadVolumes(value);
   });
+
+  model.on("change:opacity", () => {
+    let value = model.get("opacity");
+    nv.setOpacity(0, value);
+  });
+
+  model.on("change:colormap", () => {
+    let value = model.get("colormap");
+    nv.setColorMap(nv.volumes[0].id, value);
+  });
+
+  model.on("change:slice_type", () => {
+    let value = model.get("slice_type");
+    nv.setSliceType(value);
+  });
+
+  model.on("change:drag_mode", () => {
+    let value = model.get("drag_mode");
+    if (value == "DRAG_MODES.CONTRAST") {
+      nv.opts.dragMode = nv.dragModes.contrast;
+    }
+    if (value == "DRAG_MODES.MEASUREMENT") {
+      nv.opts.dragMode = nv.dragModes.measurement;
+    }
+    if (value == "DRAG_MODES.PAN") {
+      nv.opts.dragMode = nv.dragModes.pan;
+    }
+  });
+  
 }
 
 export default { render };
