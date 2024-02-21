6b6c94c68f8d5d6de64e598c4f36b26be816ea76
Trevor Manz <trevor.j.manz@gmail.com>
2024-02-21 13:16:15 -0500

    feat: add cal_min/cal_max/colorbar_visible reactive props

--- js/types.ts
+++ js/types.ts
@@ -4,6 +4,10 @@ export type VolumeModel = { model_id: string } & AnyModel<{
 	path: { name: string; data: DataView };
 	colormap: string;
 	opacity: number;
+    colorbar_visible: boolean;
+	cal_min?: number;
+	cal_max?: number;
+	visible: boolean;
 }>;
 
 export type Model = AnyModel<{
