53008fbe3ce2877f66dbb23967a048a9e6daee1d
Trevor Manz <trevor.j.manz@gmail.com>
2024-06-12 21:06:04 -0400

    chore: Migrate to typescript and add linting/typechecking

---
+++ js/volume.ts
@@ -0,0 +1,108 @@
+import * as niivue from "@niivue/niivue";
+import * as lib from "./lib.ts";
+import type { Model, VolumeModel } from "./types.ts";
+
+/**
+ * Create a new NVImage and attach the necessary event listeners
+ * Returns the NVImage and a cleanup function that removes the event listeners.
+ */
+function create_volume(
+	nv: niivue.Niivue,
+	vmodel: VolumeModel,
+): [niivue.NVImage, () => void] {
+	const volume = new niivue.NVImage(
+		vmodel.get("path").data.buffer, // dataBuffer
+		lib.unique_id(vmodel), // name
+		vmodel.get("colormap"), // colormap
+		vmodel.get("opacity"), // opacity
+		undefined, // pairedImgData
+		vmodel.get("cal_min"), // cal_min
+		vmodel.get("cal_max"), // cal_max
+		undefined, // trustMinCalMinMax
+		undefined, // percentileFrac
+		undefined, // ignoreZeroVoxels
+		undefined, // visible
+		undefined, // useQFormNotSForm
+		undefined, // colormapNegative
+		undefined, // frame4D
+		undefined, // imageType
+		undefined, // cal_minNeg
+		undefined, // cal_maxNeg
+		vmodel.get("colorbar_visible"), // colorbarVisible
+		undefined, // colormapLabel
+	);
+	function colorbar_visible_changed() {
+		volume.colorbarVisible = vmodel.get("colorbar_visible");
+		nv.updateGLVolume();
+	}
+	function cal_min_changed() {
+		volume.cal_min = vmodel.get("cal_min");
+		nv.updateGLVolume();
+	}
+	function cal_max_changed() {
+		volume.cal_min = vmodel.get("cal_min");
+		nv.updateGLVolume();
+	}
+	function colormap_changed() {
+		volume.colormap = vmodel.get("colormap");
+		nv.updateGLVolume();
+	}
+	function opacity_changed() {
+		volume.opacity = vmodel.get("opacity");
+		nv.updateGLVolume();
+	}
+	vmodel.on("change:colorbar_visible", colorbar_visible_changed);
+	vmodel.on("change:cal_min", cal_min_changed);
+	vmodel.on("change:cal_max", cal_max_changed);
+	vmodel.on("change:colormap", colormap_changed);
+	vmodel.on("change:opacity", opacity_changed);
+	return [
+		volume,
+		() => {
+			vmodel.off("change:colorbar_visible", colorbar_visible_changed);
+			vmodel.off("change:cal_min", cal_min_changed);
+			vmodel.off("change:cal_max", cal_max_changed);
+			vmodel.off("change:colormap", colormap_changed);
+			vmodel.off("change:opacity", opacity_changed);
+		},
+	];
+}
+
+export async function render_volumes(
+	nv: niivue.Niivue,
+	model: Model,
+	cleanups: Map<string, () => void>,
+) {
+	const vmodels = await lib.gather_models<VolumeModel>(
+		model,
+		model.get("_volumes"),
+	);
+	const curr_names = nv.volumes.map((v) => v.name);
+	const new_names = vmodels.map(lib.unique_id);
+	const update_type = lib.determine_update_type(curr_names, new_names);
+	if (update_type === "add") {
+		// We know that the new volumes are the same as the old volumes,
+		// except for the last one. We can just add the last volume.
+		const vmodel = vmodels[vmodels.length - 1];
+		const [volume, cleanup] = create_volume(nv, vmodel);
+		cleanups.set(volume.id, cleanup);
+		nv.addVolume(volume);
+		return;
+	}
+	// HERE can be the place to add more update types
+	// ...
+
+	// We don't know what the update type is, so we need to remove all volumes
+	// and add the new ones.
+
+	// clear all volumes
+	for (const [_, cleanup] of cleanups) cleanup();
+	cleanups.clear();
+
+	// create each volume and add one-by-one
+	for (const vmodel of vmodels) {
+		const [volume, cleanup] = create_volume(nv, vmodel);
+		cleanups.set(volume.id, cleanup);
+		nv.addVolume(volume);
+	}
+}
---
+++ js/widget.ts
@@ -0,0 +1,44 @@
+import * as niivue from "@niivue/niivue";
+import type { Model } from "./types.ts";
+
+import { render_meshes } from "./mesh.ts";
+import { render_volumes } from "./volume.ts";
+
+export default {
+	async render({ model, el }: { model: Model; el: HTMLElement }) {
+		const canvas = document.createElement("canvas");
+		const container = document.createElement("div");
+		container.style.height = "300px";
+		container.appendChild(canvas);
+		el.appendChild(container);
+
+		const nv = new niivue.Niivue(model.get("_opts") ?? {});
+		nv.attachToCanvas(canvas);
+
+		const vcleanups = new Map<string, () => void>();
+		await render_volumes(nv, model, vcleanups);
+		model.on("change:_volumes", () => render_volumes(nv, model, vcleanups));
+
+		const mcleanups = new Map<string, () => void>();
+		await render_meshes(nv, model, mcleanups);
+		model.on("change:_meshes", () => render_meshes(nv, model, mcleanups));
+
+		// Any time we change the options, we need to update the nv object
+		// and redraw the scene.
+		model.on("change:_opts", () => {
+			nv.opts = { ...nv.opts, ...model.get("_opts") };
+			nv.drawScene();
+			nv.updateGLVolume();
+		});
+
+		// All the logic for cleaning up the event listeners and the nv object
+		return () => {
+			for (const [_, cleanup] of vcleanups) cleanup();
+			vcleanups.clear();
+			for (const [_, cleanup] of mcleanups) cleanup();
+			mcleanups.clear();
+			model.off("change:_volumes");
+			model.off("change:_opts");
+		};
+	},
+};
