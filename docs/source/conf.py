796287f095bc4a39cb6813b155633f65d34cfdf0
Bradley Alford <bradleyalford1@icloud.com>
2024-11-27 06:29:21 -0500

    adjusts doc website and adds most information from README file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,25 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder."""
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'IPyNiiVue'
-project_copyright = '2024, Author Names'
-author = 'Author Names'
-release = '2.0.1'
+project = "IPyNiiVue"
+project_copyright = "2024, Author Names"
+author = "Author Names"
+release = "2.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pyramid'
-html_static_path = ['_static']
+html_theme = "pyramid"
+html_static_path = ["_static"]
