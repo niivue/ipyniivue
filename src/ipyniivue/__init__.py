85535573f73700a58e63103583ef4bf8c4aeb743
Christian O'Reilly <christian.oreilly@gmail.com>
2025-01-29 11:34:14 -0500

    Linting

--- src/ipyniivue/__init__.py
+++ src/ipyniivue/__init__.py
@@ -3,7 +3,7 @@
 import importlib.metadata
 
 from .constants import DragMode, MuliplanarType, SliceType  # noqa: F401
-from .widget import NiiVue, WidgetObserver  # noqa: F401
 from .download_dataset import download_dataset  # noqa: F401
+from .widget import NiiVue, WidgetObserver  # noqa: F401
 
 __version__ = importlib.metadata.version("ipyniivue")
