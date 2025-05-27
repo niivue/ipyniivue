b8b5adb41eadd62faad8b76d1e33b32c8ba18307
Anthony Androulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2025-05-26 22:13:53 -0400

    Update widget.ts
    organize model event handlers into attachModelEventHandlers function

--- js/widget.ts
+++ js/widget.ts
@@ -5,6 +5,54 @@ import { Disposer } from "./lib.ts";
 import { render_meshes } from "./mesh.ts";
 import { render_volumes } from "./volume.ts";
 
+// Attach model event handlers
+function attachModelEventHandlers(
+	nv: niivue.Niivue,
+	model: Model,
+	disposer: Disposer,
+) {
+	model.on("change:_volumes", () => render_volumes(nv, model, disposer));
+	model.on("change:_meshes", () => render_meshes(nv, model, disposer));
+
+	// Any time we change the options, we need to update the nv gl
+	model.on("change:_opts", () => {
+		nv.document.opts = { ...nv.opts, ...model.get("_opts") };
+		nv.updateGLVolume();
+	});
+
+	// Handle any message directions from the nv object.
+	model.on("msg:custom", (payload: CustomMessagePayload) => {
+		const { type, data } = payload;
+		switch (type) {
+			case "save_document": {
+				const { fileName, compress } = data;
+				nv.saveDocument(fileName, compress);
+				break;
+			}
+			case "save_html": {
+				const { fileName, canvasId } = data;
+				// Note: currently fails as esm is inaccesbile.
+				// nv.saveHTML(fileName, canvasId, esm);
+				break;
+			}
+			case "save_image": {
+				const { fileName, saveDrawing, indexVolume } = data;
+				nv.saveImage({
+					filename: fileName,
+					isSaveDrawing: saveDrawing,
+					volumeByIndex: indexVolume,
+				});
+				break;
+			}
+			case "save_scene": {
+				const { fileName } = data;
+				nv.saveScene(fileName);
+				break;
+			}
+		}
+	});
+}
+
 // Attach Niivue event handlers
 function attachNiivueEventHandlers(nv: niivue.Niivue, model: Model) {
 	nv.onImageLoaded = async (volume: niivue.NVImage) => {
@@ -272,55 +320,19 @@ export default {
 		const nv = new niivue.Niivue(model.get("_opts") ?? {});
 		nv.attachToCanvas(canvas);
 
+		// Attach model event handlers
+		attachModelEventHandlers(nv, model, disposer);
+
 		// Attach niivue event handlers
 		attachNiivueEventHandlers(nv, model);
 
 		await render_volumes(nv, model, disposer);
-		model.on("change:_volumes", () => render_volumes(nv, model, disposer));
 		await render_meshes(nv, model, disposer);
-		model.on("change:_meshes", () => render_meshes(nv, model, disposer));
 
 		nv.createEmptyDrawing();
 
-		// Any time we change the options, we need to update the nv gl
-		model.on("change:_opts", () => {
-			nv.document.opts = { ...nv.opts, ...model.get("_opts") };
-			nv.updateGLVolume();
-		});
 		model.on("change:height", () => {
 			container.style.height = `${model.get("height")}px`;
-		});
-
-		// Handle any message directions from the nv object.
-		model.on("msg:custom", (payload: CustomMessagePayload) => {
-			const { type, data } = payload;
-			switch (type) {
-				case "save_document": {
-					const { fileName, compress } = data;
-					nv.saveDocument(fileName, compress);
-					break;
-				}
-				case "save_html": {
-					const { fileName, canvasId } = data;
-					// Note: currently fails as esm is inaccesbile.
-					// nv.saveHTML(fileName, canvasId, esm);
-					break;
-				}
-				case "save_image": {
-					const { fileName, saveDrawing, indexVolume } = data;
-					nv.saveImage({
-						filename: fileName,
-						isSaveDrawing: saveDrawing,
-						volumeByIndex: indexVolume,
-					});
-					break;
-				}
-				case "save_scene": {
-					const { fileName } = data;
-					nv.saveScene(fileName);
-					break;
-				}
-			}
 		});
 
 		// All the logic for cleaning up the event listeners and the nv object
