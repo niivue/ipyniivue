c40cd0f31562ae5d2db908f4e6031366162d1df1
Anthony Androulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2025-06-01 02:29:56 -0400

    biome lint fixes

--- js/niivue-fix.d.ts
+++ js/niivue-fix.d.ts
@@ -1,7 +1,7 @@
 import { Niivue } from "@niivue/niivue";
 
 declare module "@niivue/niivue" {
-  interface Niivue {
-    setMeshShader(meshId: string, shader: string): void;
-  }
+	interface Niivue {
+		setMeshShader(meshId: string, shader: string): void;
+	}
 }
