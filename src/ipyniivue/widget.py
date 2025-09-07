7108b5f1cf37a04a1e7a992f68cee1df20a04177
anthonyandroulakis <43938491+AnthonyAndroulakis@users.noreply.github.com>
2025-09-07 19:45:55 -0400

    Remove ability to set config options directly
    must set via opts trait

--- src/ipyniivue/widget.py
+++ src/ipyniivue/widget.py
@@ -904,18 +904,6 @@ class NiiVue(BaseAnyWidget):
         self._event_handlers = {}
         self.on_msg(self._handle_custom_msg)
 
-    def __setattr__(self, name, value):
-        """todo: remove this starting version 2.4.1."""
-        if name in ConfigOptions.class_trait_names():
-            warnings.warn(
-                "Setting config options directly on NiiVue will not be supported "
-                f"in versions starting 2.4.1. Please use nv.opts.{name}",
-                DeprecationWarning,
-                stacklevel=2,
-            )
-            setattr(self.opts, name, value)
-        super().__setattr__(name, value)
-
     def set_state(self, state):
         """Override set_state to silence notifications for certain updates."""
         if "scene" in state:
